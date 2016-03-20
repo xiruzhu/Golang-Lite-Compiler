cb05c9ab9a30922dc3e873c6774bc0a08c53c9ea
Xiru <xiruzhu@yahoo.com>
2016-03-20 17:08:30 -0400

    more efficient runner

--- runscript.sh
+++ runscript.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 
 printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+flex go.l && bison go.y && clang lex.yy.c -o goCompiler -lfl -lm
 
 for file in $1/*.go;
 do
 	printf "On file $file\n\n"
-	./run.sh go.l go.y $file
+	cat $file | ./goCompiler
 	printf "\n\nEnd file $file\n\n"
 	printf "________________________________________________________________\n\n"
 done
